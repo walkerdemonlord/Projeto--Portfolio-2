b531eb4676cb201dcd2cd2a17113c59268b68f54
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-06-18 17:16:53 -0300

    Otimização

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-const myObserver = new IntersectionObserver( (entries) => {
-    entries.forEach( (entry) => {
-        if(entry.isIntersecting) {
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show');
         }
-    })    
-})
+    });
+});
 
 const elements = document.querySelectorAll('.hidden');
 
