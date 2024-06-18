d6967ddf2a29e6a5f611d91235904fcc54b9497c
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-06-18 18:11:33 -0300

    Menu Transparente e Conexão com os Clicks no Nav

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.header ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Previne o comportamento padrão do link
+
+            const targetId = this.getAttribute('href'); // Obtém o ID do alvo
+            const targetElement = document.querySelector(targetId); // Seleciona o elemento alvo
+
+            if (targetElement) {
+                // Adiciona um delay de 200ms (0.2 segundos)
+                setTimeout(function() {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth' // Rolagem suave
+                    });
+                }, 200); // Tempo em milissegundos
+            }
+        });
+    });
+});
+
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
